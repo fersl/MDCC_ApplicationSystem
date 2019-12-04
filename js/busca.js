2b9342f8d40de166df8278519c598ceb89ed17d4
fersl <fernandasl94@gmail.com>
2019-12-04 20:47:28 -0300

    adicionado formulário de busca

--- js/busca.js
+++ js/busca.js
@@ -2,7 +2,26 @@ new Vue ({
     el: '#app',
 
     data: {
-
+        name: '',
+        city: '',
+        uf: '',
+        institution: '',
+        interview: '',
+        area1: '',
+        area2: '',
+        pc_min: 0,
+        pc_max: 100,
+        ira_min: 0,
+        ira_max: 100,
+        uf_list: ['', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'],
+        institutions: ['', 'UFC - Fortaleza', 'UFC - Sobral', 'UFC - Russas', 'UFC - Quixadá', 'UECE', 'IFCE', 'UFCA', 'URCA', 'UVA', 'UNILAB', 'Unifor', 'FA7', 'Fanor', 'Fametro', 'Unichristus', 'FFB'],
+        courses: {
+            'UFC - Fortaleza': ['Ciência da Computação', 'Engenharia da Computação', 'Engenharia Elétrica', 'Engenharia de Telecomunicações', 'Física', 'Matemática', 'Estatística', 'Matemática Industrial'],
+            'UECE': ['Ciência da Computação'],
+            'Unifor': ['Ciência da Computação', 'Engenharia da Computação', 'Engenharia Elétrica']
+        },
+        areas: ['', 'Engenharia de Software', 'Inteligência Artificial', 'Lógica', 'Algoritmos', 'Bancos de Dados', 'Redes', 'Computação Gráfica'],
+        int_opts: ['','Presencial', 'Skype', 'Telefone', 'Hangout']
     },
 
     methods: {
--- js/form-mestrado.js
+++ js/form-mestrado.js
@@ -1,48 +1,46 @@
-// function institution_modal(){
-//     $("#inst-modal").modal('show');
-// }
-
 new Vue ({
     el: '#app',
 
     data: {
-        form: {
-            name: '',
-            birthdate: '',
-            gender: '',
-            id_type: '',
-            id_value: '',
-            nationality: '',
-            email: '',
-            phone: '',
-            cep: '',
-            address: '',
-            address_num: '',
-            nbhood: '',
-            uf: '',
-            city: '',
-            country: '',
+        name: '',
+        birthdate: '',
+        gender: '',
+        id_type: '',
+        id_value: '',
+        nationality: '',
+        email: '',
+        phone: '',
+        cep: '',
+        address: '',
+        address_num: '',
+        nbhood: '',
+        uf: '',
+        city: '',
+        country: '',
 
-            institution: '',
-            course:'',
-            period_start: '',
-            period_end: '',
-            ira: '',
-            lattes: '',
+        institution: '',
+        course:'',
+        period_start: '',
+        period_end: '',
+        ira: '',
+        lattes: '',
 
-            pc_year: '',
-            pc_mat: 0,
-            pc_prog: 0,
-            pc_tech: 0,
-            pc_total: 0,
+        pc_year: '',
+        pc_mat: 0,
+        pc_prog: 0,
+        pc_tech: 0,
+        pc_total: 0,
 
-            area1: '',
-            area2: '',
-            interview: ''
-        },
+        area1: '',
+        area2: '',
+        interview_opt: '',
+        interview_contact: '',
 
-        
-        uf_list: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'],
+        new_inst: '',
+        new_course: '',
+        course_inst: '',
+
+        uf_list: ['-', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'],
         institutions: ['UFC - Fortaleza', 'UFC - Sobral', 'UFC - Russas', 'UFC - Quixadá', 'UECE', 'IFCE', 'UFCA', 'URCA', 'UVA', 'UNILAB', 'Unifor', 'FA7', 'Fanor', 'Fametro', 'Unichristus', 'FFB'],
         courses: {
             'UFC - Fortaleza': ['Ciência da Computação', 'Engenharia da Computação', 'Engenharia Elétrica', 'Engenharia de Telecomunicações', 'Física', 'Matemática', 'Estatística', 'Matemática Industrial'],
@@ -50,13 +48,13 @@ new Vue ({
             'Unifor': ['Ciência da Computação', 'Engenharia da Computação', 'Engenharia Elétrica']
         },
         areas: ['Engenharia de Software', 'Inteligência Artificial', 'Lógica', 'Algoritmos', 'Bancos de Dados', 'Redes', 'Computação Gráfica'],
-        int_opts: ['Skype', 'Telefone', 'Hangout']
+        int_opts: ['Presencial', 'Skype', 'Telefone', 'Hangout']
         
     },
 
     methods: {
         add_pc() {
-            this.form.pc_total = this.form.pc_mat + this.form.pc_prog + this.form.pc_tech
+            this.pc_total = this.pc_mat + this.pc_prog + this.pc_tech;
         },
 
         modal_inst() {
@@ -66,6 +64,47 @@ new Vue ({
 
         modal_course() {
             $("#course-modal").modal('show');
+        },
+
+        clear_form(){
+            this.name = '';
+            this.birthdate =  '';
+            this.gender = '';
+            this.id_type = '';
+            this.id_value = '';
+            this.nationality = '';
+            this.email = '';
+            this.phone = '';
+            this.cep = '';
+            this.address = '';
+            this.address_num = '';
+            this.nbhood = '';
+            this.uf = '';
+            this.city = '';
+            this.country = '';
+    
+            this.institution = '';
+            this.course = '';
+            this.period_start = '';
+            this.period_end = '';
+            this.ira = '';
+            this.lattes = '';
+    
+            this.pc_year = '';
+            this.pc_mat = 0;
+            this.pc_prog = 0;
+            this.pc_tech = 0;
+            this.pc_total = 0;
+    
+            this.area1 = '';
+            this.area2 = '';
+            this.interview_opt = '';
+            this.interview_contact = '';
+        },
+
+        add_inst(){
+            this.institutions.push(new_inst);
+            this.courses[new_inst] = [];
         }
     }
 });
